fc2de6f94946a0a36305424edd857d79a8d859c5
Kapranchikova Alice <kapranchikovaal@gmail.com>
2020-05-02 16:48:42 +0300

    Add eruda. Change create elements

--- game.ts
+++ game.ts
@@ -15,6 +15,23 @@ class Game {
         // Create canvas and engine.
         this._canvas = document.getElementById(canvasElement) as HTMLCanvasElement;
         this._engine = new BABYLON.Engine(this._canvas, true);
+        console.log('construtor')
+        console.log(window.DeviceMotionEvent)
+        window.addEventListener("devicemotion", this.handleOrientation, true);
+    }
+
+    handleOrientation(event) {
+        // var absolute = event.absolute;
+        // var alpha    = event.alpha;
+        // var beta     = event.beta;
+        // var gamma    = event.gamma;
+        // console.log(alpha, beta, gamma)
+
+        const x = event.accelerationIncludingGravity.x;
+        const y = event.accelerationIncludingGravity.y;
+        const z = event.accelerationIncludingGravity.z;
+        console.log(x, y, z);
+        // Do stuff with the new orientation data
     }
 
     createScene(): void {
@@ -45,67 +62,96 @@ class Game {
         this._camera.setTarget(BABYLON.Vector3.Zero());
 
         // Attach the camera to the canvas.
-        this._camera.attachControl(this._canvas, false);
+        // this._camera.attachControl(this._canvas, false);
 
         // Create a basic light, aiming 0,1,0 - meaning, to the sky.
         this._light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), this._scene);
 
-        this.addElement(3, 2, 3, 5, 5);
-        this.addElement(3, 1, 3, 5, 10);
-        this.addElement(3, 2, 3, 5, 15);
-        this.addElement(3, 1, 3, 5, 25);
+        const countEL = 100;
+        let i = 0;
 
-        // Create a built-in "ground" shape.
+        const getRandomInt = (min, max) => Math.random() * (max - min) + min;
+
+        const interval = setInterval(() => {
+            if (i >= countEL) {
+                clearInterval(interval);
+            }
+            this.addElement(i, 2, 3, Math.abs(this._camera.orthoBottom) + getRandomInt(0, 6), getRandomInt(-3, 3));
+            i++;
+        }, 100);
+
+        this.addWalls();
+        // this.addAxis();
+
+
+        // let acl = new Accelerometer({frequency: 60});
+        //
+        // acl.addEventListener('reading', () => {
+        //     console.log("Acceleration along the X-axis " + acl.x);
+        //     console.log("Acceleration along the Y-axis " + acl.y);
+        //     console.log("Acceleration along the Z-axis " + acl.z);
+        // });
+        //
+        // acl.start();
+
+
+
+
+    }
+
+    addWalls() {
         let ground = BABYLON.MeshBuilder.CreateBox('ground1',
-            {width: Math.abs(this._camera.orthoLeft) * 2, height: 2, depth: 20}, this._scene);
+            {width: Math.abs(this._camera.orthoLeft) * 2, height: 2, depth: 3}, this._scene);
         ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, {
             mass: 0,
-            restitution: 0.9
+            restitution: 0.3
         }, this._scene);
 
         ground.position.y = this._camera.orthoBottom;
 
 
         let wallLeft = BABYLON.MeshBuilder.CreateBox('wallLeft',
-            {width: 1, height: Math.abs(this._camera.orthoBottom) * 2, depth: 6}, this._scene);
+            {width: 1, height: Math.abs(this._camera.orthoBottom) * 2, depth: 3}, this._scene);
         wallLeft.physicsImpostor = new BABYLON.PhysicsImpostor(wallLeft, BABYLON.PhysicsImpostor.BoxImpostor, {
             mass: 0,
-            restitution: 0.9
+            restitution: 0.3
         }, this._scene);
 
         wallLeft.position.x = Math.abs(this._camera.orthoLeft);
 
 
         let wallRight = BABYLON.MeshBuilder.CreateBox('wallRight',
-            {width: 1, height: Math.abs(this._camera.orthoBottom) * 2, depth: 6}, this._scene);
+            {width: 1, height: Math.abs(this._camera.orthoBottom) * 2, depth: 3}, this._scene);
         wallRight.physicsImpostor = new BABYLON.PhysicsImpostor(wallRight, BABYLON.PhysicsImpostor.BoxImpostor, {
             mass: 0,
-            restitution: 0.9
+            restitution: 0.3
         }, this._scene);
 
         wallRight.position.x = this._camera.orthoLeft;
 
 
-
         let wallBack = BABYLON.MeshBuilder.CreateBox('wallBack',
-            {width: Math.abs(this._camera.orthoLeft) * 2, height: Math.abs(this._camera.orthoBottom) * 2, depth: 1, faceColors: [new BABYLON.Color4(0, 0, 0, 1)]}, this._scene);
+            {
+                width: Math.abs(this._camera.orthoLeft) * 2,
+                height: Math.abs(this._camera.orthoBottom) * 2,
+                depth: 1,
+                faceColors: [new BABYLON.Color4(0, 0, 0, 1)]
+            }, this._scene);
         wallBack.physicsImpostor = new BABYLON.PhysicsImpostor(wallBack, BABYLON.PhysicsImpostor.BoxImpostor, {
             mass: 0,
             restitution: 0.9
         }, this._scene);
         //
-        wallBack.position.z = -3;
-        this.addAxis();
-
+        wallBack.position.z = -1.5;
     }
 
     addElement(id, diameter = 2, mass = 2, y = 3, x = 0) {
         let sphere = BABYLON.MeshBuilder.CreateSphere('sphere' + id,
-            {segments: 16, diameter: diameter}, this._scene);
+            {segments: 5, diameter: diameter}, this._scene);
 
         sphere.physicsImpostor = new BABYLON.PhysicsImpostor(sphere, BABYLON.PhysicsImpostor.SphereImpostor, {
             mass: 1,
-            restitution: 0.3
+            restitution: 0.2
         }, this._scene);
 
         sphere.position.y = y;
