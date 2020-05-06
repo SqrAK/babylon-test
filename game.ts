4ee836ff610dc4da8ef66736144c22cbec77c847
SqrAK <kapranchickova@yandex.ru>
2020-05-06 18:36:18 +0300

    Add fullscreen. Add texture. Change styles for phone

--- game.ts
+++ game.ts
@@ -17,6 +17,7 @@ class Game {
         this._engine = new BABYLON.Engine(this._canvas, true);
         console.log('construtor')
         console.log(window.DeviceOrientationEvent)
+        console.log(window.DeviceMotionEvent)
         console.log(window);
         window.screen.orientation.onchange = () => {console.log('change')}
         window.addEventListener('devicemotion', this.handleOrientation, true);
@@ -51,11 +52,11 @@ class Game {
         this._scene.enablePhysics(gravityVector, physicsPlugin);
 
         // Create a FreeCamera, and set its position to (x:0, y:5, z:-10).
-        this._camera = new BABYLON.ArcRotateCamera('camera1', Math.PI / 2, Math.PI / 2, 10,
+        this._camera = new BABYLON.ArcRotateCamera('camera1', Math.PI / 2, Math.PI / 2, 100,
             new BABYLON.Vector3(0, 0, 0), this._scene);
         this._camera.mode = BABYLON.Camera.ORTHOGRAPHIC_CAMERA;
 
-        var distance = 500;
+        var distance = 100;
         var aspect = this._scene.getEngine().getRenderingCanvasClientRect().height / this._scene.getEngine().getRenderingCanvasClientRect().width;
         this._camera.orthoLeft = -distance / 2;
         this._camera.orthoRight = distance / 2;
@@ -69,7 +70,8 @@ class Game {
         this._camera.attachControl(this._canvas, false);
 
         // Create a basic light, aiming 0,1,0 - meaning, to the sky.
-        this._light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, this._camera.orthoTop, 500), this._scene);
+        this._light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1.5 * Math.abs(this._camera.orthoTop), 50), this._scene);
+        this._light = new BABYLON.HemisphericLight('light2', new BABYLON.Vector3(0, -1.5 * Math.abs(this._camera.orthoTop), 50), this._scene);
 
 
 
@@ -86,78 +88,83 @@ class Game {
             if (i >= countEL) {
                 clearInterval(interval);
             }
-            this.addElement(i, 2, 3, Math.abs(this._camera.orthoBottom) + getRandomInt(0, 6), getRandomInt(-3, 3), grass0);
+            this.addElement(i, 2, 10, Math.abs(this._camera.orthoBottom) + getRandomInt(0, 10), getRandomInt(-4, 4), grass0);
             i++;
         }, 100);
 
         this.addWalls();
-        this.addAxis();
-
-
-        // let acl = new Accelerometer({frequency: 60});
-        //
-        // acl.addEventListener('reading', () => {
-        //     console.log("Acceleration along the X-axis " + acl.x);
-        //     console.log("Acceleration along the Y-axis " + acl.y);
-        //     console.log("Acceleration along the Z-axis " + acl.z);
-        // });
-        //
-        // acl.start();
-
-
-
+        // this.addAxis();
+        this.toggleFullScreen();
 
     }
 
     addWalls() {
         let ground = BABYLON.MeshBuilder.CreateBox('ground1',
-            {width: Math.abs(this._camera.orthoLeft) * 2, height: 2, depth: 3}, this._scene);
+            {width: Math.abs(this._camera.orthoLeft) * 2, height: 2, depth: 5}, this._scene);
         ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, {
             mass: 0,
             restitution: 0.3
         }, this._scene);
 
-        ground.position.y = this._camera.orthoBottom;
+        ground.position.y = this._camera.orthoBottom - 1;
 
 
         let wallLeft = BABYLON.MeshBuilder.CreateBox('wallLeft',
-            {width: 1, height: Math.abs(this._camera.orthoBottom) * 2, depth: 3}, this._scene);
+            {width: 1, height: Math.abs(this._camera.orthoBottom) * 2, depth: 5}, this._scene);
         wallLeft.physicsImpostor = new BABYLON.PhysicsImpostor(wallLeft, BABYLON.PhysicsImpostor.BoxImpostor, {
             mass: 0,
             restitution: 0.3
         }, this._scene);
 
-        wallLeft.position.x = Math.abs(this._camera.orthoLeft);
+        wallLeft.position.x = Math.abs(this._camera.orthoLeft) + 1;
 
 
         let wallRight = BABYLON.MeshBuilder.CreateBox('wallRight',
-            {width: 1, height: Math.abs(this._camera.orthoBottom) * 2, depth: 3}, this._scene);
+            {width: 1, height: Math.abs(this._camera.orthoBottom) * 2, depth: 5}, this._scene);
         wallRight.physicsImpostor = new BABYLON.PhysicsImpostor(wallRight, BABYLON.PhysicsImpostor.BoxImpostor, {
             mass: 0,
             restitution: 0.3
         }, this._scene);
 
-        wallRight.position.x = this._camera.orthoLeft;
+        wallRight.position.x = this._camera.orthoLeft - 1;
 
 
-        // let wallBack = BABYLON.MeshBuilder.CreateBox('wallBack',
-        //     {
-        //         width: Math.abs(this._camera.orthoLeft) * 2,
-        //         height: Math.abs(this._camera.orthoBottom) * 2,
-        //         depth: 1,
-        //         faceColors: [new BABYLON.Color4(0.2666, 0.33333, 0.3529, 1)]
-        //     }, this._scene);
-        // wallBack.physicsImpostor = new BABYLON.PhysicsImpostor(wallBack, BABYLON.PhysicsImpostor.BoxImpostor, {
-        //     mass: 0,
-        //     restitution: 0.9
-        // }, this._scene);
-        // //
-        // wallBack.position.z = -1.5;
+        let wallBack = BABYLON.MeshBuilder.CreateBox('wallBack',
+            {
+                width: Math.abs(this._camera.orthoLeft) * 2 + 2,
+                height: Math.abs(this._camera.orthoBottom) * 2,
+                depth: 1
+            }, this._scene);
+        wallBack.physicsImpostor = new BABYLON.PhysicsImpostor(wallBack, BABYLON.PhysicsImpostor.BoxImpostor, {
+            mass: 0,
+            restitution: 0.9
+        }, this._scene);
+
+        const grass0 = new BABYLON.StandardMaterial("grass0", this._scene);
+        grass0.diffuseTexture = new BABYLON.Texture("textures/back.jpg", this._scene);
+        wallBack.material = grass0;
+
+        wallBack.position.z = -2.5;
     }
 
-    addElement(id, diameter = 2, mass = 2, y = 3, x = 0, texture?) {
+    toggleFullScreen() {
+        var doc = window.document;
+        var docEl = doc.documentElement;
+
+        var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+        var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+        if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+            requestFullScreen.call(docEl);
+        }
+        else {
+            cancelFullScreen.call(doc);
+        }
+    }
+
+    addElement(id, diameter = 3, mass = 2, y = 3, x = 0, texture?) {
         let sphere = BABYLON.MeshBuilder.CreateSphere('sphere' + id,
-            {segments: 5, diameter: diameter}, this._scene);
+            {segments: 2, diameter: diameter}, this._scene);
 
         sphere.physicsImpostor = new BABYLON.PhysicsImpostor(sphere, BABYLON.PhysicsImpostor.SphereImpostor, {
             mass: 1,
